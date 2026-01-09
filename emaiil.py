7083005ca18bfd6826169a6f8c3e10e031dc3780
sagarit <founder.evidora@gmail.com>
2026-01-09 18:07:14 +0530

    Add files via upload

---
+++ emaiil.py
@@ -0,0 +1,100 @@
+from langchain_groq import ChatGroq
+
+from langgraph.graph import StateGraph,END,START
+from typing import TypedDict,List
+import os
+from dotenv import load_dotenv
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+load_dotenv()  
+model = ChatGroq(
+    model="llama-3.1-8b-instant",
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+
+class AgentState(TypedDict):
+    from_mail: str
+    to_mail: str
+    subject: str
+    prompt: str
+    message: str
+    status: str
+
+
+
+
+def write_email(state: AgentState) -> dict:
+    prompt = f"""
+You are a world-class professional email writer with 15+ years of experience.
+
+Write a professional email about:
+{state["prompt"]}
+
+Rules:
+- Include a clear SUBJECT LINE at the top
+- Then the email body
+- No markdown
+- No explanations
+
+Format EXACTLY like this:
+
+Subject: <subject here>
+
+<email body>
+"""
+
+    result = model.invoke(prompt)
+    text = result.content.strip()
+
+    subject, body = text.split("\n", 1)
+    subject = subject.replace("Subject:", "").strip()
+
+    return {
+        "subject": subject,
+        "message": body.strip()
+    }
+
+def send_email(state: AgentState) -> dict:
+    msg = MIMEMultipart()
+    msg["From"] = state["from_mail"]
+    msg["To"] = state["to_mail"]
+    msg["Subject"] = state["subject"]
+
+    msg.attach(MIMEText(state["message"], "plain"))
+
+    try:
+        server = smtplib.SMTP(os.getenv("SMTP_HOST"), int(os.getenv("SMTP_PORT")))
+        server.starttls()
+        server.login(os.getenv("SMTP_USER"), os.getenv("SMTP_PASS"))
+        server.send_message(msg)
+        server.quit()
+
+        return {"status": "sent"}
+
+    except Exception as e:
+        return {"status": f"failed: {str(e)}"}
+
+graph = StateGraph(AgentState)
+
+graph.add_node("write_email", write_email)
+graph.add_node("send_email", send_email)
+
+graph.add_edge(START, "write_email")
+graph.add_edge("write_email", "send_email")
+graph.add_edge("send_email", END)
+
+app = graph.compile()
+
+initial_state = {
+    "from_mail": "founder.evidora@gmail.com",
+    "to_mail": "digiance.sagarit@gmail.com",
+    "prompt": "my name is sagarit and recievers name is abc write that Reschedule today's 2pm meeting to 5pm due to urgent code bugs",
+}
+
+result = app.invoke(initial_state)
+
+print("STATUS:", result["status"])
